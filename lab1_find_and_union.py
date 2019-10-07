b1219799b30566f4232cac7f0a7bc87bff130ac3
dimazhylko <zhylko.dima@gmail.com>
2019-10-08 01:09:16 +0200

    bugfix

--- lab1_find_and_union.py
+++ lab1_find_and_union.py
@@ -1,11 +1,17 @@
 from dimacs import *
+import sys
+
+sys.setrecursionlimit(100000)
+
+p = dict()
 
 def union(x, y):
 	global p
-	a= p[x]
-	b=p[y]
+	a = find(x)
+	b = find(y)
 
-	p[a] = b
+	if(a!=b):
+		p[a] = b
 
 def find(x):
 	global p
@@ -16,23 +22,42 @@ def find(x):
 	else:
 		return x
 
-(V, L) = loadWeightedGraph("graphs/g1")
+def run(file):
+	global p
+	(V, L) = loadWeightedGraph(file)
 
 
-L_s = [(c, x, y) for (x,y,c) in L]
-L_s.sort(reverse=True)
+	L_s = [(c, x, y) for (x,y,c) in L]
+	L_s.sort(reverse=True)
 
-s = 1
-t = 2
+	s = 1
+	t = 2
 
-p = [i for i in range(V+1)] 
+	p = {i:i for i in range(1, V+1)}
 
-m = -1
-for (c, x, y) in L_s:
+	m = -1
+	for (c, x, y) in L_s:
 
-	union(x, y)
-	m = c
-	if(find(s) == find(t)):
-		break
+		union(x, y)
+		
+		if(find(s) == find(t)):
+			m=c
+			break
 
-print(m)
+	return m
+
+import os
+files = os.listdir("graphs/")
+print(files)
+c=0
+for i in files:
+	res = -1
+	with open('graphs/' + i) as f:
+		res = int(f.readline().split()[3])
+	res_t = run('graphs/' + i)
+	if(res == res_t):
+		c+=1
+		print(i + ": OK")
+	else:
+		print(i + ": answer is: " + str(res) + " found: " + str(res_t))
+print(str(c) + "/" + str(len(files)))
